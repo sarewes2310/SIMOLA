830e388d314d74ba7099406744c31610355ec045
Sarewes2310 <sase240@students.unnes.ac.id>
2019-02-12 13:40:40 +0700

    add model

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -18,6 +18,12 @@ class User extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
+
+	public function __construct(){
+        parent::__construct();
+        header('Access-Control-Allow-Origin: *');
+    }
+	
 	public function index()
 	{
 		$this->load->model("UserModel");
