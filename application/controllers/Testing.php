9b3c53205b44a6538d35a39477f897d4c0929048
Sarewes2310 <sase240@students.unnes.ac.id>
2019-02-18 20:15:50 +0700

    menambahkan kelas controller testing

--- application/controllers/Testing.php
+++ application/controllers/Testing.php
@@ -17,7 +17,7 @@ class Testing extends CI_Controller {
         # $this->load->view('login');
         # set_time_limit(1);
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://simolasocket-nodejs.herokuapp.com/");
+        curl_setopt($ch, CURLOPT_URL, "https://simolasocket-nodejs.herokuapp.com/home");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
         $output = curl_exec($ch);
         var_dump($output);
