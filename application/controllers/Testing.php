f87bd0284edee266fbc67ca1bbd4390de0fc598a
Sarewes2310 <sase240@students.unnes.ac.id>
2019-02-21 22:17:29 +0700

    tambah library dropbox dan menambah fungsi testing

--- application/controllers/Testing.php
+++ application/controllers/Testing.php
@@ -34,7 +34,30 @@ class Testing extends CI_Controller {
     }
 
     public function getDropBoxAT(){
-        echo $_SERVER['HTTP_HOST']; 
-        echo $_SERVER['QUERY_STRING'];
+        #echo $_SERVER['HTTP_HOST']; 
+        #echo $_SERVER['QUERY_STRING'];
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://api.dropboxapi.com/oauth2/token",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => "code=".substr($_SERVER['QUERY_STRING'],5)."&grant_type=authorization_code&redirect_uri=https%3A%2F%2Fsimola.herokuapp.com%2F",
+            CURLOPT_HTTPHEADER => array(
+                "Authorization: Basic NTJ1OW13eGxjeGd2MWoyOmV0cG1vYjkwOW0yM3hlMg==",
+                "cache-control: no-cache"
+            ),
+        ));
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        curl_close($curl);
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            echo $response;
+        }
     }
 }
