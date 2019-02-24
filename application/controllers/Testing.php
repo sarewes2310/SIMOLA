f06adaa7954bd78ebe82ccf9fbc93e5859ced365
Sarewes2310 <sase240@students.unnes.ac.id>
2019-02-24 11:21:37 +0700

    edit function getfromserverjs

--- application/controllers/Testing.php
+++ application/controllers/Testing.php
@@ -27,6 +27,7 @@ class Testing extends CI_Controller {
     
     public function getFromServerJS(){
         var_dump($_POST);
+        var_dump($_GET);
         foreach($_POST as $val){
             echo $val;
         }
