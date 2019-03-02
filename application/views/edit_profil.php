d26bab76ea2296c16bc7e086c839b0c6f50a30c7
Sarewes2310 <sase240@students.unnes.ac.id>
2019-03-02 20:26:22 +0700

    edit tampilan edit_profil.php

--- application/views/edit_profil.php
+++ application/views/edit_profil.php
@@ -2,19 +2,25 @@
 echo json_encode('
 <form action="https://lembarkerjasiswa.herokuapp.com/index.php/Login/submitGuru" method="post" accept-charset="utf-8" id="guruLogin">
     <div class="form-group row">
-	    <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+	    <label for="nama" class="col-sm-2 col-form-label">Nama</label>
 	    <div class="col-sm-10">
 	    	<input type="text" class="form-control" id="nama" placeholder="Nama">
 	    </div>
 	</div>
     <div class="form-group row">
-		<label for="inputEmail3" class="col-sm-2 col-form-label">Username</label>
+		<label for="username" class="col-sm-2 col-form-label">Username</label>
 		<div class="col-sm-10">
 			<input type="text" class="form-control" id="username" placeholder="Username">
 	    </div>
+    </div>
+    <div class="form-group row">
+	    <label for="email" class="col-sm-2 col-form-label">Email</label>
+	    <div class="col-sm-10">
+	    	<input type="email" class="form-control" id="email" placeholder="Email">
+	    </div>
 	</div>
 	<div class="form-group row">
-	    <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+	    <label for="password" class="col-sm-2 col-form-label">Password</label>
 	    <div class="col-sm-10">
 	    	<input type="password" class="form-control" id="password" placeholder="Password">
 	    </div>
