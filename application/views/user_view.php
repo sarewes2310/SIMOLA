a6f5c86e3e4456f05d304b126bf111aa11438576
Sarewes2310 <sase240@students.unnes.ac.id>
2019-03-03 00:27:29 +0700

    edit tampilan userview.php

--- application/views/user_view.php
+++ application/views/user_view.php
@@ -3,8 +3,8 @@ echo json_encode('
 <style>
 .float{
 	position:fixed;
-	width:60px;
-	height:60px;
+	width:55px;
+	height:55px;
 	bottom:20px;
 	right:20px;
 	background-color:#0C9;
@@ -21,6 +21,9 @@ echo json_encode('
     padding: 0px;
 }
 </style>
+<script>
+    const inputModal = d
+</script>
 <table class="table">
   <tbody>
     <tr>
@@ -52,8 +55,28 @@ echo json_encode('
     </tr>
   </tbody>
 </table>
-<a href="#" class="float">
+<a class="float" id="tambahUserModal" data-toggle="modal" data-target="#inputModal">
     <i class="fa fa-plus my-float"></i>
 </a>
+<!-- Modal -->
+<div class="modal fade" id="inputModal" tabindex="-1" role="dialog" aria-labelledby="inputModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 ',JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_QUOT | JSON_HEX_APOS);
 ?>
