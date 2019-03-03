e1705d035e82040f8b9388a29e57be391b1e2002
Sarewes2310 <sase240@students.unnes.ac.id>
2019-03-03 07:47:50 +0700

    edit file login.js

--- application/views/login.php
+++ application/views/login.php
@@ -10,7 +10,6 @@
     <link href="https://simola.herokuapp.com/assets/css/sigin.css" rel="stylesheet">
     <!--<link rel="manifest" href="assets/manifest.json">-->
     <script src="<?php echo base_url()?>assets/js/variabel_utama.js" type="text/javascript" charset="utf-8"></script>
-    <script src="<?php echo base_url()?>assets/js/login.js" type="text/javascript" charset="utf-8"></script>
     <style>
         .bd-placeholder-img {
             font-size: 1.125rem;
@@ -50,37 +49,6 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-<script>
-var form = document.querySelector('form');
-form.addEventListener('submit', function(e){
-    const hasil = {
-        'username' : document.getElementById('username_login').value,
-        'password' : document.getElementById('password_login').value
-    };
-    console.log(hasil);
-    fetch(base_url+'DataUser/submitUserLogin',
-    {
-        method : 'post',
-        body : parserData(hasil),
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-        }
-    }).then(function(res){
-       return res.json(); 
-    }).then(function(response){
-        console.log(response);
-    });
-    return e.preventDefault();
-});
-
-function parserData(data){
-	hasil = '';
-	for(var i in data){
-		hasil = hasil+(i+'='+data[i]);
-		hasil = hasil+'&';
-	}
-	return new URLSearchParams(hasil)
-}
-</script>
+<script src="<?php echo base_url()?>assets/js/login.js" type="text/javascript" charset="utf-8"></script>
 </body>
 </html>
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,5 +1,35 @@
-var form = document.querySelector('form');
 
 function submitLogin(){
     
+}
+
+var form = document.querySelector('form');
+form.addEventListener('submit', function(e){
+    const hasil = {
+        'username' : document.getElementById('username_login').value,
+        'password' : document.getElementById('password_login').value
+    };
+    console.log(hasil);
+    fetch(base_url+'DataUser/submitUserLogin',
+    {
+        method : 'post',
+        body : parserData(hasil),
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        }
+    }).then(function(res){
+       return res.json(); 
+    }).then(function(response){
+        console.log(response);
+    });
+    return e.preventDefault();
+});
+
+function parserData(data){
+	hasil = '';
+	for(var i in data){
+		hasil = hasil+(i+'='+data[i]);
+		hasil = hasil+'&';
+	}
+	return new URLSearchParams(hasil)
 }
