a7dc420d0ef930388f2aa3987d81b93aa00409b3
Sarewes2310 <sase240@students.unnes.ac.id>
2019-03-03 07:20:16 +0700

    edit file login.js

--- application/views/login.php
+++ application/views/login.php
@@ -9,6 +9,7 @@
     <!-- Custom styles for this template -->
     <link href="https://simola.herokuapp.com/assets/css/sigin.css" rel="stylesheet">
     <!--<link rel="manifest" href="assets/manifest.json">-->
+    <script src="<?php echo base_url()?>assets/js/variabel_utama.js" type="text/javascript" charset="utf-8"></script>
     <style>
         .bd-placeholder-img {
             font-size: 1.125rem;
