72f225fbcf6e4a90967ecaa1b67a3f4968a6ff1d
Sarewes2310 <sase240@students.unnes.ac.id>
2019-03-03 07:36:01 +0700

    edit file login.js

--- application/views/login.php
+++ application/views/login.php
@@ -50,5 +50,36 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+<script>
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    const hasil = {
+        'username' : document.getElementById('username_login').value,
+        'password' : document.getElementById('password_login').value
+    };
+    console.log(hasil);
+    fetch(base_url+'datauser/submitUserLogin',
+    {
+        method : 'post',
+        body : parserData(hasil),
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        }
+    }).then(function(res){
+       return res.json(); 
+    }).then(function(response){
+        console.log(response);
+    });
+});
+
+function parserData(data){
+	hasil = '';
+	for(var i in data){
+		hasil = hasil+(i+'='+data[i]);
+		hasil = hasil+'&';
+	}
+	return new URLSearchParams(hasil)
+}
+</script>
 </body>
 </html>
--- assets/js/login.js
+++ assets/js/login.js
@@ -3,32 +3,3 @@ var form = document.querySelector('form');
 function submitLogin(){
     
 }
-form.addEventListener('submit', function(e){
-    e.preventDefault();
-    const hasil = {
-        'username' : document.getElementById('username_login').value,
-        'password' : document.getElementById('password_login').value
-    };
-    console.log(hasil);
-    fetch(base_url+'datauser/submitUserLogin',
-    {
-        method : 'post',
-        body : parserData(hasil),
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-        }
-    }).then(function(res){
-       return res.json(); 
-    }).then(function(response){
-        console.log(response);
-    });
-});
-
-function parserData(data){
-	hasil = '';
-	for(var i in data){
-		hasil = hasil+(i+'='+data[i]);
-		hasil = hasil+'&';
-	}
-	return new URLSearchParams(hasil)
-}
