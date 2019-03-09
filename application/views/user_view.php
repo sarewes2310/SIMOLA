943a3ccb8540ce83a31bad66f7e4c5de36368ad6
Sarewes2310 <sase240@students.unnes.ac.id>
2019-03-09 21:23:51 +0700

    edit User.php

--- application/views/user_view.php
+++ application/views/user_view.php
@@ -13,7 +13,6 @@ foreach($data as $key => $value){
             <td style="width:80%">
                 '.$value['nama'].'
             </td>
-            <td>'.$stat.'</td>
             <td><button type="button" class="btn btn-outline-success" data-toggle="modal" data-target="#editModal" onclick="return peringatan('.$value['idus'].')">Edit</button></td>
             <td><button type="button" class="btn btn-outline-danger" data-toggle="modal" data-target="#deleteModal" onclick="return peringatan('.$value['idus'].')">Delete</button></td>        
         </tr>
