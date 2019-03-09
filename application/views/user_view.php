15095e56ab2470a4e226ec7b3bbcc90c8695fbd1
Sarewes2310 <sase240@students.unnes.ac.id>
2019-03-09 20:48:45 +0700

    edit User.php

--- application/views/user_view.php
+++ application/views/user_view.php
@@ -1,15 +1,13 @@
 <?php
 $dataT = '';
-foreach($data as $dataH){
-    foreach($dataH as $key => $value){
-        $dataT .= '
-            <tr>
-                <td style="width:80%">'.$value['nama'].'</td>
-                <td><button type="button" class="btn btn-outline-success" data-toggle="modal" data-target="#editModal" onclick="return peringatan('.$value['idus'].')">Edit</button></td>
-                <td><button type="button" class="btn btn-outline-danger" data-toggle="modal" data-target="#deleteModal" onclick="return peringatan('.$value['idus'].')">Delete</button></td>        
-            </tr>
-        ';
-    }
+foreach($data as $key => $value){
+    $dataT .= '
+        <tr>
+            <td style="width:80%">'.$value['nama'].'</td>
+            <td><button type="button" class="btn btn-outline-success" data-toggle="modal" data-target="#editModal" onclick="return peringatan('.$value['idus'].')">Edit</button></td>
+            <td><button type="button" class="btn btn-outline-danger" data-toggle="modal" data-target="#deleteModal" onclick="return peringatan('.$value['idus'].')">Delete</button></td>        
+        </tr>
+    ';
 }
 echo json_encode('
 <style>
