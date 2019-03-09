3db6ba3acc8acd2b8c53989df6174d00eeeda883
Sarewes2310 <sase240@students.unnes.ac.id>
2019-03-09 11:34:00 +0700

    edit getviewProfil

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -62,7 +62,8 @@ class User extends CI_Controller
             $hasil = $this->UserModel->getViewEditProfilM($this->input->post('idus'));
             $h['data'] = $hasil;
             $this->load->view('edit_profil');
-        }#var_dump($this->input->post('idus'));
+        }
+        var_dump($this->input->post('idus'));
     }
 
     public function getViewDropbox()
