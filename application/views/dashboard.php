2917285a0bf8d4162963505e54619dd149382b8c
Sarewes2310 <sase240@students.unnes.ac.id>
2019-03-12 13:03:06 +0700

    edit file dashboard.php

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -153,6 +153,7 @@
         function updateUIForPushEnabled(currentToken) {
             fetch('https://simolasocket-nodejs.herokuapp.com/addPushUser?us='+currentToken,{
                 method : 'GET',
+                mode: 'cors'
             }).then(response => {
                 return response.json();
             }).then(hasil => {
