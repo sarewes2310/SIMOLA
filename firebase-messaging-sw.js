9ab33c472afdf16ce5c268e2cd2a9a324303f14f
Sarewes2310 <sase240@gmail.com>
2019-09-20 12:57:24 +0700

    update version cache

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -71,8 +71,8 @@ self.addEventListener('push', function(event) {
     );
 });
 
-var CACHE_STATIC_NAME = 'static-v7';
-var CACHE_DYNAMIC_NAME = 'dynamic-v7';
+var CACHE_STATIC_NAME = 'static-v8';
+var CACHE_DYNAMIC_NAME = 'dynamic-v8';
 
 var STATIC_FILES = [
     'https://simola.herokuapp.com/manifest.json',
